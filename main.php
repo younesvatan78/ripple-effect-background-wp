5cb3909a858380df4c5b991eafd76bb91ec9f7aa
Younes <younesvatan78@gmail.com>
2020-10-28 09:25:16 +0330

    Files pushed to repo

---
+++ main.php
@@ -0,0 +1,266 @@
+<?php
+/*
+Plugin Name: Ripple Effect Background
+Description: Add ripple effect to where ever you need.
+Text Domain: ripple-effect-background-wp
+Author: Younes Vatankhah 
+Author URI: https://www.zhaket.com/web/artman-wordpress-theme
+License: GPLv2
+Version: 1.0.0
+
+*/
+defined( 'ABSPATH' ) or die( 'Unauthorized Access!' );
+
+   
+  
+
+
+  function wp_rippleeffect_add_menu_page(){
+    add_menu_page(
+    __('Ripple Effect Background', 'ripple-effect-backgroud-wp'),
+    __('Ripple Effect Background', 'ripple-effect-backgroud-wp'),
+    'manage_options',
+    'wp_rippleeffect_background',
+    'wp_rippleeffect_render_menu_page',
+    'dashicons-image-filter',
+    99
+    );
+}
+add_action( 'admin_menu', 'wp_rippleeffect_add_menu_page' );
+
+function wp_rippleeffect_admin_enqueue_scripts(){
+    wp_enqueue_style('wp_rippleeffect_admin-css-style',plugins_url('assets/css/admin.css',__FILE__));
+}
+add_action('admin_enqueue_scripts','wp_rippleeffect_admin_enqueue_scripts');
+
+
+
+
+
+function wp_rippleeffect_render_menu_page() {
+	
+	
+
+	wp_enqueue_script( 'ripple_effect-admin-js', ripple_effect_PLUGINS_URL . '/assets/js/admin.js');
+
+	wp_enqueue_script('media-library',plugins_url('assets/js/media-library.js', __FILE__ ), array( 'jquery' ));
+	 
+	$nonce =  empty( $_POST['ripple_effect-nonce'] ) ? false : sanitize_text_field( $_POST['ripple_effect-nonce'] );
+	$nonce_ok = wp_verify_nonce( $nonce, 'ripple-effect-dash' );
+	
+	
+	$enable_front_page = get_option( 'ripple_effect-enable_front_page', false );
+	$enable_blog_page = get_option( 'ripple_effect-enable_blog_page', false );
+		
+	if( $nonce_ok ) {
+		
+		$first_text = empty( $_POST['first_text'] ) ? false : wp_kses_post( stripslashes_deep( $_POST['first_text'] ) );
+
+		$animated_text = empty( $_POST['animated_text'] ) ? false : wp_kses_post( stripslashes_deep( $_POST['animated_text'] ) );
+
+		$second_text = empty( $_POST['second_text'] ) ? false : wp_kses_post( stripslashes_deep( $_POST['second_text'] ) );
+
+		
+		$img = empty( $_POST['ripple_effect']['select_img_ripple_effect'] )? false: $img = sanitize_text_field($_POST['ripple_effect']['select_img_ripple_effect']); 
+
+		$btn1_label = empty( $_POST['ripple_effect']['btn1_label'] ) ? false: $btn1_label = sanitize_text_field( $_POST['ripple_effect']['btn1_label'] );
+
+		$btn2_label = empty( $_POST['ripple_effect']['btn2_label'] ) ? false: $btn2_label = sanitize_text_field( $_POST['ripple_effect']['btn2_label'] );
+		
+		$btn1_link = empty( $_POST['ripple_effect']['btn1_link'] ) ? false: $btn1_link = sanitize_text_field( $_POST['ripple_effect']['btn1_link'] );
+
+		$btn2_link = empty( $_POST['ripple_effect']['btn2_link'] ) ? false: $btn2_link = sanitize_text_field( $_POST['ripple_effect']['btn2_link'] );
+
+		
+		
+		$enable_front_page = empty( $_POST['ripple_effect']['enable_front_page'] ) ? false : true;
+		$enable_blog_page = empty( $_POST['ripple_effect']['enable_blog_page'] ) ? false : true;
+		
+		update_option( 'animated_text',$animated_text );
+		update_option( 'first_text',$first_text );
+		update_option( 'second_text',$second_text );
+		update_option( 'select_img_ripple_effect',$img );		
+		update_option( 'ripple_effect-enable_front_page', $enable_front_page );
+		update_option( 'ripple_effect-enable_blog_page', $enable_blog_page );
+		update_option( 'btn1_label',$btn1_label );
+		update_option( 'btn1_link',$btn1_link );
+		update_option( 'btn2_label',$btn2_label );
+		update_option( 'btn2_link',$btn2_link );
+		
+
+
+	}
+	
+	if ( !empty( $_POST ) && !$nonce_ok ) {
+		echo '<div class="ripple-effect-dash error"><p>لطفا دوباره لوگین کنید اطلاعات ذخیره نشد</p></div>';
+	}
+	
+?>
+
+<form method='post' id='ripple_effect_admin_settings_form'>
+	<?php wp_nonce_field( 'ripple-effect-dash', 'ripple_effect-nonce' ) ?>
+	<div class='ripple-effect-dash'>
+		<h1>پس زمینه با افکت موج</h1>
+		
+		<p><button type='submit' class='button button-primary' style='min-width: 140px;'> ذخیره تغییرات</button></p>
+	</div>
+	<div class='ripple-effect-dash'>		
+		<h2>انتخاب مکان و یا استفاده از کد کوتاه</h2>
+		<div class='setting'>
+			<label>کد کوتاه 
+				<input type='text' value='[ripple-effect-background]' onclick='this.select()' readonly >
+			</label>
+		</div>
+	</div>
+	<div class='ripple-effect-dash'>
+		<h2>اطلاعات را وارد کنید</h2>
+		<div class='setting'>
+			<h3 style="font-size: 15px;">تصویر پس زمینه</h3>
+			<?php
+			$value = get_option( 'select_img_ripple_effect' ); ?>
+				<input id="upload_image" type="hidden" size="36" name="ripple_effect[select_img_ripple_effect]"value="<?php echo $value; ?>" />
+
+				<img id="imgClickAndChange" style="display:flex; max-height: 300px; max-width: 300px" src="<?php  	echo $value; ?>">
+
+				<div style = "display: block">
+					<input id="upload_image_button" style="margin : 20px 10px!important" class="button"      	type="button" value="انتخاب تصویر" />
+
+					<input id="remove_image_button"style="margin : 20px 10px!important" class="button" type="button" value="پاک کردن تصویر" />
+				</div>
+
+
+				<div>
+					<h2>متن شماره یک</h2>
+					<?php
+					$first_text = stripslashes_deep(get_option('first_text'));
+					wp_editor( $first_text, 'first_text' );?>
+				</div>
+
+				<div>
+					<br>
+					<h2>متن متحرک</h2>
+					<i>برای نمایش جداگانه هر متن بعد از هر متن Enter بزنید</i>
+					<?php
+					$animated_text = stripslashes_deep(get_option('animated_text'));
+					wp_editor( $animated_text, 'animated_text' );?>
+				</div>
+
+
+				<div>
+					<br>
+					<h2>متن شماره دو</h2>
+					<?php
+					$second_text = stripslashes_deep(get_option('second_text'));
+					wp_editor( $second_text, 'second_text' );?>
+				</div>
+
+				<div class="btn-box">
+					<?php 
+					$value2_label = get_option('btn2_label');
+					$value2_link = get_option('btn2_link');?>
+
+					<h3>دکمه یک</h3>
+					<label class="label">متن دکمه</label>
+					<input type="text" name="ripple_effect[btn2_label]" value="<?php echo $value2_label; ?>">
+
+					<label class="label">لینک دکمه</label>
+					<input type="url" name="ripple_effect[btn2_link]" value="<?php echo $value2_link; ?>">
+					<?php
+					$value1 = get_option('btn1_label');
+					$value2 = get_option('btn1_link'); ?>
+
+					<h3>دکمه دو</h3>
+					<label class="label">متن دکمه</label>
+					<input type="text" name="ripple_effect[btn1_label]" value="<?php echo $value1; ?>">
+
+					<label class="label">لینک دکمه</label>
+					<input type="url" name="ripple_effect[btn1_link]" value="<?php echo $value2; ?>">			
+				</div>
+		</div>
+	</div>
+</form>
+
+<?php 
+}
+	
+	add_action('wp_enqueue_scripts','ripple_effect_scripts');
+	add_shortcode( 'ripple-effect-background','ripple_effect_display');
+	
+	function ripple_effect_display( $atts,$content = null ) {
+		
+		
+		$first_text = stripslashes_deep(get_option('first_text'));
+		$animated_text = stripslashes_deep(get_option('animated_text'));
+		$second_text = stripslashes_deep(get_option('second_text'));
+		$btn1_label = stripslashes_deep(get_option('btn1_label'));
+		$btn2_label = stripslashes_deep(get_option('btn2_label'));
+		$btn1_link = stripslashes_deep(get_option('btn1_link'));
+		$btn2_link = stripslashes_deep(get_option('btn2_link'));
+
+
+		$img = sanitize_text_field(get_option('select_img_ripple_effect'));
+		wp_enqueue_script( 'ripple-effect-main-js' );
+		wp_enqueue_script( 'typed-js' );
+		wp_enqueue_script( 'ripples' );
+		wp_enqueue_script( 'jquery-ripple-effect' );
+		
+		$if = function ($condition, $true, $false) { return isset($condition) ? $true : $false; };
+		
+		
+		if(!$first_text): $first_text_has_value = null; else: $first_text_has_value = $first_text; endif;
+
+		if(!$animated_text): $animated_has_value = null; else: $animated_has_value = '<div class="text-right" id="typed-strings">'.$animated_text.'</div><h3><span id="typed"></span></h3>'; endif;
+
+		if(!$second_text): $second_text_has_value = null; else: $second_text_has_value = $second_text; endif;
+
+		if(!$btn1_label): $btn_one_has_value = null; else: $btn_one_has_value = '<a href="'.$btn1_link.'" class="btn-ripple-effect view-demo">'.$btn1_label.'</a>'; endif;
+
+		if(!$btn2_label): $btn_two_has_value = null; else: $btn_two_has_value = '<a href="'.$btn2_link.'" class="btn-ripple-effect buy-now" data-scroll-nav="1">'.$btn2_label.'</a>'; endif;
+
+
+		
+		return <<<HTML
+		<section class="hero-area-fix" style="background: url($img) no-repeat scroll center center;">
+		<div class="hero-area" id="water">
+			<div class="container">
+				<div class="row-ripple-effect">
+					<div class="hero-text">
+						{$first_text}
+						{$animated_has_value}
+						<div style="direction:rtl;">{$second_text}</div>
+						{$btn_two_has_value}
+						{$btn_one_has_value}
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	HTML;
+	}
+	
+
+	
+
+
+	function ripple_effect_scripts() {
+		wp_enqueue_style( 'pg-style',plugins_url( 'assets/css/style.css', __FILE__ ), array());
+		
+		
+
+		wp_enqueue_script('typed-js', plugins_url( 'assets/js/typed.min.js', __FILE__ ), array( 'jquery' ),'',true);
+
+		wp_enqueue_script('jquery-ripple-effect', plugins_url( 'assets/js/jquery-2.2.4.min.js', __FILE__ ), array( 'jquery' ),'',true);
+
+		wp_enqueue_script('ripples', plugins_url( 'assets/js/jquery.ripples-min.js', __FILE__ ), array( 'jquery' ),'',true);
+
+
+		wp_enqueue_script('ripple-effect-main-js', plugins_url( 'assets/js/main.js', __FILE__ ), array( 'jquery' ),'',true);
+		
+	}
+	
+
+
+
+
+
+?>
